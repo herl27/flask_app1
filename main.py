7a05e0400f5dad0d43e7c3ef4ebd517ea0a6448b
her <lihe92@outlook.com>
2018-03-26 16:49:47 +0800

    3d

--- main.py
+++ main.py
@@ -1,16 +1,28 @@
 from flask import Flask, render_template
 from flask_bootstrap import Bootstrap
-from flask.ext.moment import Moment
+from flask_moment import Moment
 from datetime import datetime
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import Required
+
+class NameForm(FlaskForm):
+    name = StringField("您的名字是：", validators=[Required()])
+    submit = SubmitField('提交')
 
 app = Flask(__name__)
-
 bootstrap = Bootstrap(app)
 moment = Moment(app)
+app.config['SECRET_KEY'] = 'hard to guess'
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html',current_time=datetime.utcnow())
+    name = None
+    form = NameForm()
+    if form.validate_on_submit():
+        name = form.name.data
+        form.name.data = ''
+    return render_template('index.html',current_time=datetime.utcnow(), name=name, form=form)
 
 
 @app.route('/user/<name>')
